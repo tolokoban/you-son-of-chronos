f513a54dfdfaadcaaaa19ec0897ae7453579321c
Tolokoban <contact@tolokoban.org>
2020-04-27 11:13:25 +0200

    Updates

---
+++ precache-manifest.7d6fb53ee0ecc40749e175fecf6ba1a8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "519cca1186f20687274c8aabbe0276d6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "29dbbd4a31844c36a84b",
+    "url": "./static/css/2.7582ca43.chunk.css"
+  },
+  {
+    "revision": "183694e92714000a5fa6",
+    "url": "./static/css/main.8816da08.chunk.css"
+  },
+  {
+    "revision": "29dbbd4a31844c36a84b",
+    "url": "./static/js/2.bc85a7c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.bc85a7c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "183694e92714000a5fa6",
+    "url": "./static/js/main.b8ecdb08.chunk.js"
+  },
+  {
+    "revision": "c78d31fdc84bb5558805",
+    "url": "./static/js/runtime-main.884a770a.js"
+  },
+  {
+    "revision": "99be908bf9c2d4217ea763e64f5694d6",
+    "url": "./static/media/dialog.99be908b.yaml"
+  },
+  {
+    "revision": "d9c85352c2c1b6d28bbc80b1bede207b",
+    "url": "./static/media/input-date.d9c85352.yaml"
+  },
+  {
+    "revision": "ed7190eadf3846a05e7238597dfe0c3b",
+    "url": "./static/media/input-image.ed7190ea.yaml"
+  },
+  {
+    "revision": "7270440b8ddb42ebfe9fba9297a3563d",
+    "url": "./static/media/josefin.7270440b.woff2"
+  },
+  {
+    "revision": "04c7e99b2918bcab6bebc7a5c44459c5",
+    "url": "./static/media/mystery-quest-400.04c7e99b.woff2"
+  },
+  {
+    "revision": "d73f403cb7dedbcc76e42e5d81cb4e9b",
+    "url": "./static/media/wheel.d73f403c.png"
+  }
+]);
